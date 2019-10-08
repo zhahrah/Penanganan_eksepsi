22e4142e685523912ae96253753cebe9f35c5680
zhahrah <49123237+zhahrah@users.noreply.github.com>
2019-10-08 15:50:36 +0700

    Create try-except2.py

---
+++ try-except2.py
@@ -0,0 +1,19 @@
+def main():
+    print("PROGRAM PEMBAGIAN BILANGAN")
+
+    a = float(input("Masukkan a: "))
+    b = float(input("Masukkan b: "))
+
+
+    try :
+        hasil = a / b
+
+    except ZeroDivisionError:
+        print("\nERROR: Nilai b tidak boleh nol")
+        print("\na : ", a)
+        print("b : ", b)
+
+        print("a / b = ", hasil)
+
+if __name__ == "__main__" :
+    main()
